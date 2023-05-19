9f84c78f886443946e87cd267dc17d5d18f4edee
Marcella Vizoná Vasconcelos <126676999+MarcellaVizona@users.noreply.github.com>
2023-05-19 16:12:50 -0300

    commit-inicial

---
+++ script.js
@@ -0,0 +1,9 @@
+// var valor = confirm("Vamos juntos aprender a programar em JS?")
+//  if(valor ==true ) {
+//              alert("Parábens, vamos lá!!!");
+//  } else { 
+//     alert("Opoção errada, aperte F5 para recaregar a página ")
+//  }
+
+ var email = prompt(" Digite seu e-mail");
+ alert("O e-mail  " + email + "  foi coletado com sucesso.")
