1ebf5df8888ca9b498fb86750afc8ecfb07f542b
Marcella Vizoná Vasconcelos <126676999+MarcellaVizona@users.noreply.github.com>
2023-05-24 15:05:43 -0300

    commit

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-// var valor = confirm("Vamos juntos aprender a programar em JS?")
-//  if(valor ==true ) {
-//              alert("Parábens, vamos lá!!!");
-//  } else { 
-//     alert("Opoção errada, aperte F5 para recaregar a página ")
-//  }
+/* var valor = confirm("Vamos juntos aprender a programar em JS?")
+  if(valor ==true ) {
+              alert("Parábens, vamos lá!!!");
+  } else { 
+     alert("Opoção errada, aperte F5 para recaregar a página ")
+  }*/
 
- var email = prompt(" Digite seu e-mail");
- alert("O e-mail  " + email + "  foi coletado com sucesso.")
+ /* var email = prompt(" Digite seu e-mail");
+  
+  if(email == null || email == ""){
+     alert(" Não foi possivel coletar seu e-mail");
+  }
+  else {
+    alert("O e-mail  " + email + "  foi coletado com sucesso.");
+  }*/
+
+
+ var prova = parseFloat(prompt (" Qual a nota da prova? "));
+ var trabalho = parseFloat(prompt("Qual a nota do trabalho?"));
+
+ var notaFinal = prova + trabalho;
+
+ alert(notaFinal);
